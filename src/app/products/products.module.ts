39fd7b44f2e3b258a5978d88f20689e163cbb9c6
Ghaouti BENKEBIL <ghaouti.benkebil@capgemini.com>
2025-06-17 15:09:47 +0200

    Structure Improvement: Product

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { AllProductsComponent } from './components/all-products/all-products.component';
 import { ProductsDetailsComponent } from './components/products-details/products-details.component';
 import {SharedModule} from '../shared/shared.module';
+import { ProductComponent } from './components/product/product.component';
 
 
 
 @NgModule({
   declarations: [
     AllProductsComponent,
-    ProductsDetailsComponent
+    ProductsDetailsComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NgOptimizedImage
   ]
 })
 export class ProductsModule { }
